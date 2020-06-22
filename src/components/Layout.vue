95cc647c2157e88b08e83f2484c700c94ebc04f9
Echo <395150457@qq.com>
2020-06-22 17:20:23 +0800

    1

---
+++ src/components/Layout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="layout">
+    <div class="logo"><img src="./images/logo.png" alt=""></div>
+    <div class="menu">
+      <ul class="menulist">
+        <li>
+          <span class="text">首页</span>
+          <div class="line"></div>
+        </li>
+        <li>
+          <span class="text">产品展示</span>
+          <div class="line"></div>
+        </li>
+        <li class="on">
+          <span class="text">服务流程</span>
+          <div class="line"></div>
+        </li>
+        <li>
+          <span class="text">关于我们</span>
+          <div class="line"></div>
+        </li>
+        <li>
+          <span class="text">联系我们</span>
+          <div class="line"></div>
+        </li>
+      </ul>
+    </div>
+    <div class="menuIcon">
+      <i class="fa fa-bars fa-2x  " aria-hidden="true"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Layout"
+  }
+</script>
+
+<style lang="less" scoped>
+  @import url('./less/layout');
+</style>
